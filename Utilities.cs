82e6ebc121e84a10eaa94e1f224dfc222eb6d521
Joe Castro <Joe.Castro@hbo.com>
2013-03-28 17:45:06 -0700

    Further splitting Standard libraries into Windows specific
    Refactoring to make it so I have a clean set of files from
    the standard namespace that will work on Mono.

--- Utilities.cs
+++ Utilities.cs
@@ -7,7 +7,6 @@ namespace Standard
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
     using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
     using System.IO;
@@ -26,7 +25,6 @@ namespace Standard
 
     internal static partial class Utility
     {
-        private static readonly Version _osVersion = Environment.OSVersion.Version;
         private static readonly Random _randomNumberGenerator = new Random();
 
         [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
@@ -293,18 +291,6 @@ namespace Standard
             return objectType.GetInterfaces().Any(type => type == interfaceType);
         }
 
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        public static bool IsOSVistaOrNewer
-        {
-            get { return _osVersion >= new Version(6, 0); }
-        }
-
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        public static bool IsOSWindows7OrNewer
-        {
-            get { return _osVersion >= new Version(6, 1); }
-        }
-
         /// <summary>
         /// Wrapper around File.Copy to provide feedback as to whether the file wasn't copied because it didn't exist.
         /// </summary>
@@ -360,40 +346,6 @@ namespace Standard
             }
         }
 
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        public static void SafeDestroyIcon(ref IntPtr hicon)
-        {
-            IntPtr p = hicon;
-            hicon = IntPtr.Zero;
-            if (IntPtr.Zero != p)
-            {
-                NativeMethods.DestroyIcon(p);
-            }
-        }
-
-        /// <summary>GDI's DeleteObject</summary>
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        public static void SafeDeleteObject(ref IntPtr gdiObject)
-        {
-            IntPtr p = gdiObject;
-            gdiObject = IntPtr.Zero;
-            if (IntPtr.Zero != p)
-            {
-                NativeMethods.DeleteObject(p);
-            }
-        }
-
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        public static void SafeDestroyWindow(ref IntPtr hwnd)
-        {
-            IntPtr p = hwnd;
-            hwnd = IntPtr.Zero;
-            if (NativeMethods.IsWindow(p))
-            {
-                NativeMethods.DestroyWindow(p);
-            }
-        }
-
 
         [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         public static void SafeDispose<T>(ref T disposable) where T : IDisposable
@@ -404,56 +356,6 @@ namespace Standard
             if (null != t)
             {
                 t.Dispose();
-            }
-        }
-
-        /// <summary>GDI+'s DisposeImage</summary>
-        /// <param name="gdipImage"></param>
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        public static void SafeDisposeImage(ref IntPtr gdipImage)
-        {
-            IntPtr p = gdipImage;
-            gdipImage = IntPtr.Zero;
-            if (IntPtr.Zero != p)
-            {
-                NativeMethods.GdipDisposeImage(p);
-            }
-        }
-
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        [SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands")]
-        public static void SafeCoTaskMemFree(ref IntPtr ptr)
-        {
-            IntPtr p = ptr;
-            ptr = IntPtr.Zero;
-            if (IntPtr.Zero != p)
-            {
-                Marshal.FreeCoTaskMem(p);
-            }
-        }
-
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        [SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands")]
-        public static void SafeFreeHGlobal(ref IntPtr hglobal)
-        {
-            IntPtr p = hglobal;
-            hglobal = IntPtr.Zero;
-            if (IntPtr.Zero != p)
-            {
-                Marshal.FreeHGlobal(p);
-            }
-        }
-
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        [SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands")]
-        public static void SafeRelease<T>(ref T comObject) where T : class
-        {
-            T t = comObject;
-            comObject = default(T);
-            if (null != t)
-            {
-                Assert.IsTrue(Marshal.IsComObject(t));
-                Marshal.ReleaseComObject(t);
             }
         }
 
